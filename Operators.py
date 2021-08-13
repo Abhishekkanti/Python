50371f60842d91c841c9250db0c51f5114751080
Abhishekkanti <88873697+Abhishekkanti@users.noreply.github.com>
2021-08-13 12:09:04 +0530

    Operator.py

---
+++ Operators.py
@@ -0,0 +1,20 @@
+x = 2
+y = 4
+
+#Addition
+print ("\n Sum of x&y=",x + y)
+
+#Subtraction
+print("\n Subtraction of x&y=",x - y)
+
+#Multiplication
+print("\n Multipe of x&y=",x * y)
+
+#Division
+print("\n Division of x&y=",y / x)
+
+#Modulo
+print ("\n Modular of x&y=",y % x)
+
+#Power
+print("\n Power of x&y=",y ** x)
