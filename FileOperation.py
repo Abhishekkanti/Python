82df20c538eb43c4e787e91d7f720ab04aaf61af
Abhishekkanti <88873697+Abhishekkanti@users.noreply.github.com>
2021-08-25 13:07:25 +0530

    Create FileOperation.py

---
+++ FileOperation.py
@@ -0,0 +1,19 @@
+#Writing in file 
+
+with open("text.txt", "w",encoding='utf-8')as f:
+    f.write("now wrote in file")
+    f.close()
+    f = open("text.txt", "r")
+print(f.read())
+
+
+##Reading file
+
+f = open('text.txt','r',encoding='utf-8')
+print(f.read())
+# closing opened file
+f = open("text.txt", "r",encoding='utf-8')
+print(f.read())
+f.close()
+    
+
